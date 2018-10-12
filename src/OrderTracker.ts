517a82ad550bb0e300b1d36c9c003319d0328850
Henry Harder <henry@paradigm.market>
2018-10-12 13:11:26 -0700

    consistency

--- src/OrderTracker.ts
+++ src/OrderTracker.ts
@@ -14,6 +14,7 @@
 import { EventEmitter } from "events";
 
 export class OrderTracker {
+    
     private em: EventEmitter; // event emiter instance
     private orders: Array<object>; // stores valid orders
 
